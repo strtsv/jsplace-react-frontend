3bcfd19e46db51049d162c83e029138b3bba9365
jsusilo <enr@bk.ru>
2020-07-11 22:39:40 +0500

    Kinds component

---
+++ src/components/Home/Kinds.js
@@ -0,0 +1,199 @@
+import React from "react";
+
+class Kinds extends React.Component {
+  render() {
+    return (
+      <section className="section section-lg section-lg-1 bg-gray-700 context-dark">
+        <div className="container">
+          <div className="row row-60 justify-content-between">
+            <div className="col-lg-7 col-xl-6">
+              <div className="row row-40 row-lg-0 row-flex number-counter">
+                <div
+                  className="col-sm-6 col-lg-6 wow fadeInLeft"
+                  data-wow-delay="0.2s"
+                >
+                  <div className="blurb-hover">
+                    <div className="blurb-hover-header">
+                      <div className="blurb-hover-icon linearicons-users2" />
+                      <div className="blurb-hover-counter index-counter" />
+                    </div>
+                    <h5 className="blurb-hover-title">Qualified Team</h5>
+                    <p className="blurb-hover-exeption">
+                      Our company is comprised of the best developers.
+                    </p>
+                  </div>
+                </div>
+                <div className="col-sm-6 col-lg-6 wow fadeInLeft">
+                  <div className="blurb-hover">
+                    <div className="blurb-hover-header">
+                      <div className="blurb-hover-icon linearicons-file-check" />
+                      <div className="blurb-hover-counter index-counter" />
+                    </div>
+                    <h5 className="blurb-hover-title">Quality Assurance</h5>
+                    <p className="blurb-hover-exeption">
+                      Our team checks every script submitted by users.
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="col-sm-6 col-lg-6 wow fadeInLeft"
+                  data-wow-delay="0.4s"
+                >
+                  <div className="blurb-hover">
+                    <div className="blurb-hover-header">
+                      <div className="blurb-hover-icon linearicons-smartphone-embed" />
+                      <div className="blurb-hover-counter index-counter" />
+                    </div>
+                    <h5 className="blurb-hover-title">Flawless Code</h5>
+                    <p className="blurb-hover-exeption">
+                      We guarantee that our scripts code is clean.
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="col-sm-6 col-lg-6 wow fadeInLeft"
+                  data-wow-delay="0.2s"
+                >
+                  <div className="blurb-hover">
+                    <div className="blurb-hover-header">
+                      <div className="blurb-hover-icon linearicons-lamp" />
+                      <div className="blurb-hover-counter index-counter" />
+                    </div>
+                    <h5 className="blurb-hover-title">Lots of Scripts</h5>
+                    <p className="blurb-hover-exeption">
+                      We have scripts for a variety of purposes available on our
+                      site.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-5 col-xl-5">
+              <h3 className="font-weight-bold">
+                All kinds of
+                <br className="d-none d-xl-block" /> scripts for you
+              </h3>
+              <div
+                className="card-group-custom number-counter-1"
+                id="accordion1"
+                role="tablist"
+                aria-multiselectable="false"
+              >
+                {}
+                <article className="card card-custom">
+                  <div className="card-header" role="tab">
+                    <div className="card-title">
+                      <a
+                        id="accordion1-card-head-aellxsll"
+                        data-toggle="collapse"
+                        data-parent="#accordion1"
+                        href="#accordion1-card-body-waanntaf"
+                        aria-controls="accordion1-card-body-waanntaf"
+                        aria-expanded="true"
+                        role="button"
+                      >
+                        <span className="index-counter" />
+                        <span className="title">Animations</span>
+                        <div className="card-arrow" />
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    className="collapse show"
+                    id="accordion1-card-body-waanntaf"
+                    aria-labelledby="accordion1-card-head-aellxsll"
+                    data-parent="#accordion1"
+                    role="tabpanel"
+                  >
+                    <div className="card-body">
+                      <p>
+                        Animation is a great tool to make your website brighter
+                        and more attractive. They are extremely popular among
+                        mobile apps, as it’s a crucial part of the user
+                        experience.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+                {}
+                <article className="card card-custom card-secondary-2">
+                  <div className="card-header" role="tab">
+                    <div className="card-title">
+                      <a
+                        className="collapsed"
+                        id="accordion1-card-head-juiopkcn"
+                        data-toggle="collapse"
+                        data-parent="#accordion1"
+                        href="#accordion1-card-body-dxvrrifd"
+                        aria-controls="accordion1-card-body-dxvrrifd"
+                        aria-expanded="false"
+                        role="button"
+                      >
+                        <span className="index-counter" />
+                        <span className="title">Transitions</span>
+                        <div className="card-arrow" />
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    className="collapse"
+                    id="accordion1-card-body-dxvrrifd"
+                    aria-labelledby="accordion1-card-head-juiopkcn"
+                    data-parent="#accordion1"
+                    role="tabpanel"
+                  >
+                    <div className="card-body">
+                      <p>
+                        Transitions are the animated changes between two pages,
+                        states or views to provide visual continuity to the user
+                        interface. They can use any kind of visual effects.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+                {}
+                <article className="card card-custom card-secondary-3">
+                  <div className="card-header" role="tab">
+                    <div className="card-title">
+                      <a
+                        className="collapsed"
+                        id="accordion1-card-head-mrymovbc"
+                        data-toggle="collapse"
+                        data-parent="#accordion1"
+                        href="#accordion1-card-body-fschhnon"
+                        aria-controls="accordion1-card-body-fschhnon"
+                        aria-expanded="false"
+                        role="button"
+                      >
+                        <span className="index-counter" />
+                        <span className="title">Vector Graphics</span>
+                        <div className="card-arrow" />
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    className="collapse"
+                    id="accordion1-card-body-fschhnon"
+                    aria-labelledby="accordion1-card-head-mrymovbc"
+                    data-parent="#accordion1"
+                    role="tabpanel"
+                  >
+                    <div className="card-body">
+                      <p>
+                        Transitions are the animated changes between two pages,
+                        states or views to provide visual continuity to the user
+                        interface. They can use any kind of visual effects.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Kinds;
