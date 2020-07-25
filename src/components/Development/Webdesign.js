0ed0c1f82d948c64a1e4f9a02d0501ed5dbe03d7
jsusilo <enr@bk.ru>
2020-07-26 00:01:53 +0500

    Webdesign

---
+++ src/components/Development/Webdesign.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Webdesign extends React.Component {
+  render() {
+    return (
+      <section className="section section-lg section-lg-2 bg-gray-700">
+        <div className="container">
+          <div className="row row-40 justify-content-between">
+            <div className="col-lg-11">
+              <h3 className="font-weight-bold custom-title">Web design</h3>
+              <p className="big">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nis
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Webdesign;
